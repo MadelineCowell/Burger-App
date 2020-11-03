4d27b31c10eb8a8722ea2a7c2bf89bc41c69e53b
MadelineC <madeline.cowell@colorado.edu>
2020-11-03 07:57:47 -0700

    Creating mvc framework

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('The Classic');
+INSERT INTO burger (name) VALUES ('Blackend Blue');
+INSERT INTO burger (name, devoured) VALUES ('Shroom Luvas', true);
+INSERT INTO burger (name, devoured) VALUES ('The Tap', true);
+INSERT INTO burger (name, devoured) VALUES ('BBQ Bacon Cheeseburger', true);
+INSERT INTO burger (name) VALUES ('Green Chili');
